9187e51a6f2fa33023693e9b61b94d8ecc95aab3
Jonas Leitner <jonas.leitner93@gmail.com>
2020-09-24 23:26:30 +0200

    finished contact form

--- static/contact/contact.php
+++ static/contact/contact.php
@@ -2,39 +2,39 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
+require 'vendor/autoload.php';
 
 $configs = include('config.php');
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Mailer = "smtp";
+set_time_limit(60); // set the time limit to 120 seconds
 
-$mail->SMTPDebug  = 1;
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port       = 587;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = $configs["mail"];
-$mail->Password   = $configs["password"];
+$Mail = new PHPMailer();
+$Mail->IsSMTP(); // Use SMTP
+$Mail->Timeout = 120;
+$Mail->Host        = "smtp.gmail.com"; // Sets SMTP server
+# $Mail->SMTPDebug   = 2; // 2 to enable SMTP debug information
+$Mail->SMTPAuth    = TRUE; // enable SMTP authentication
+$Mail->Port        = 587; // set the SMTP port
+$Mail->Username   = $configs["mail"];
+$Mail->Password   = $configs["password"];
+$Mail->Priority    = 1; // Highest priority - Email priority (1 = High, 3 = Normal, 5 = low)
+$Mail->CharSet     = 'UTF-8';
+$Mail->Encoding    = '8bit';
+$Mail->Subject = "Kontaktanfrage via jonasleitner.de";
+$Mail->ContentType = 'text/html; charset=utf-8\r\n';
+$Mail->From        = $configs["mail"];
+$Mail->FromName    = $_POST['name'];
+$Mail->WordWrap    = 900; // RFC 2822 Compliant for Max 998 characters per line
 
-if(isset($_POST['submit'])){
+$Mail->AddAddress( $configs['mail'] ); // To:
+$Mail->isHTML( FALSE );
+$Mail->Body    = $_POST['email'] . " schrieb: \n" . $_POST['message'];
+$Mail->Send();
+$Mail->SmtpClose();
 
-    $mail->IsHTML(true);
-    $mail->AddAddress($configs["mail"], "Jonas Leitner");
-    $mail->SetFrom($configs["mail"], $_POST['name']);
-    $mail->Subject = "Kontaktanfrage via jonasleitner.de";
-    $content = $_POST['message'];
-
-    $mail->MsgHTML($content);
-    if(!$mail->Send()) {
-        echo "Fehler beim Versenden";
-        var_dump($mail);
-    } else {
-        echo "Danke für deine Mail! Ich werde dir in Kürze antworten.";
-    }
-
-    }
+if ( $Mail->IsError() ) {
+    echo "Fehler beim Versenden";
+} else {
+    echo "Danke für deine Mail! Ich werde dir in Kürze antworten.";
+}
 ?>
